cc41dd89e63dab1f10cafe10e2db3a46a4e82ff5
Agustin Daguerre <agustin.s.daguerre@gmail.com>
2015-03-02 22:36:43 -0200

    gitignore

---
+++ Obligatorio/config/parametros.php
@@ -0,0 +1,8 @@
+<?php
+
+    define("CANTPAG", 8);
+    define("DRIVER", "mysql");
+    define("SERVIDOR", "localhost");
+    define("BASE", "videoProducer");
+    define("USUARIO", "root");
+    define("CLAVE", "oso2203");
