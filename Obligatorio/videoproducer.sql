aa050bd8b6448a0ec53bdc04427aaa5e3bb0da20
Agustin Daguerre <agustin.s.daguerre@gmail.com>
2015-03-10 18:25:11 -0300

    Refactor DB script - Drop and Recreates DB

--- Obligatorio/videoproducer.sql
+++ Obligatorio/videoproducer.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS `videoproducer`;
+CREATE DATABASE  IF NOT EXISTS `videoproducer` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `videoproducer`;
+
 -- phpMyAdmin SQL Dump
 -- version 2.10.3
 -- http://www.phpmyadmin.net
@@ -86,24 +90,24 @@ CREATE TABLE `videos` (
   `description` longtext,
   `client` varchar(45) NOT NULL,
   `releaseDate` date NOT NULL,
-  `rating` decimal(10,2) unsigned NOT NULL,
-  `votes` int(10) unsigned NOT NULL,
+  `rating` decimal(10,2) unsigned NOT NULL default '0',
+  `votes` int(10) unsigned NOT NULL default '0',
   `deleted` binary(1) NOT NULL,
   PRIMARY KEY  (`idVideo`),
   UNIQUE KEY `url_UNIQUE` (`url`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=26 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- 
 -- Dumping data for table `videos`
 -- 
 
-INSERT INTO `videos` VALUES (22, 1, 'F14n63OM3go', 0, 'Bordo vs 12', 'Bordo vs 12', 'Bordo vs 12', '2015-10-01',0,0, 0x30);
-INSERT INTO `videos` VALUES (23, 0, 'ArislVlGea4', 0, 'Bordo vs Yaguari', 'Bordo vs Yaguari', 'Bordo vs Yaguari', '2015-10-01',0,0, 0x30);
-INSERT INTO `videos` VALUES (24, 0, 'mJVDeIwSEnw', 0, 'Bordo vs Wanderers', 'Bordo vs Wanderers', 'Bordo vs Wanderers', '2015-10-01',0,0, 0x30);
-INSERT INTO `videos` VALUES (25, 0, 'CmWaRy31eSA', 0, 'Bordo vs Matadero', 'Bordo vs Matadero', 'Bordo vs Matadero', '2015-10-01',0,0, 0x30);
-INSERT INTO `videos` VALUES (17, 0, 'DlJEt2KU33I', 0, '2014 World Cup', '2014 World Cup', 'John Oliver Brazil World Cup', '2015-10-01',0,0, 0x30);
-INSERT INTO `videos` VALUES (18, 0, 'ybyxjlas4Z0', 0, 'Bordo premios', 'Bordo premios', 'Bordo Premios', '2015-10-01',0,0, 0x30);
-INSERT INTO `videos` VALUES (19, 0, '00gzNbI9_a4', 0, 'Bordo vs OCC', 'Bordo vs OCC', 'Bordo vs OCC', '2015-10-01',0,0, 0x30);
-INSERT INTO `videos` VALUES (20, 0, 'nnDfGTGzNDY', 0, 'Bordo vs Odonto', 'Bordo vs Odonto', 'Bordo vs Odontologia', '2015-10-01',0,0, 0x30);
-INSERT INTO `videos` VALUES (21, 0, 'ZNpMJGLHDGQ', 0, 'Bordo vs Moron', 'Bordo vs Moron', 'Bordo vs Moron', '2015-10-01',0,0, 0x30);
-INSERT INTO `videos` VALUES (16, 0, 'Y0oX0xiwOv8', 0,'Boyhood Trailer', 'Boyhood is nominated for a total of 6 Academy Awards including Best Picture, Best Actor in a Supporting Role (Ethan Hawke), Best Actress in a Supporting Role (Patricia Arquette), Best Director (Richard Linklater), Best Film Editing (Sandra Adair) and Best Original Screenplay (Richard Linklater). ', 'Boyhood | Official US Trailer | 2015 Oscar No', '2015-10-01',0,0, 0x30);
+INSERT INTO `videos` VALUES (NULL, 1, 'F14n63OM3go', default, 'Bordo vs 12', 'Bordo vs 12', 'Bordo vs 12', '2015-10-01',default,default, 0x30);
+INSERT INTO `videos` VALUES (NULL, 0, 'ArislVlGea4', default, 'Bordo vs Yaguari', 'Bordo vs Yaguari', 'Bordo vs Yaguari', '2015-10-01',default,default, 0x30);
+INSERT INTO `videos` VALUES (NULL, 0, 'mJVDeIwSEnw', default, 'Bordo vs Wanderers', 'Bordo vs Wanderers', 'Bordo vs Wanderers', '2015-10-01',default,default, 0x30);
+INSERT INTO `videos` VALUES (NULL, 0, 'CmWaRy31eSA', default, 'Bordo vs Matadero', 'Bordo vs Matadero', 'Bordo vs Matadero', '2015-10-01',default,default, 0x30);
+INSERT INTO `videos` VALUES (NULL, 0, 'DlJEt2KU33I', default, '2014 World Cup', '2014 World Cup', 'John Oliver Brazil World Cup', '2015-10-01',default,default, 0x30);
+INSERT INTO `videos` VALUES (NULL, 0, 'ybyxjlas4Z0', default, 'Bordo premios', 'Bordo premios', 'Bordo Premios', '2015-10-01',default,default, 0x30);
+INSERT INTO `videos` VALUES (NULL, 0, '00gzNbI9_a4', default, 'Bordo vs OCC', 'Bordo vs OCC', 'Bordo vs OCC', '2015-10-01',default,default, 0x30);
+INSERT INTO `videos` VALUES (NULL, 0, 'nnDfGTGzNDY', default, 'Bordo vs Odonto', 'Bordo vs Odonto', 'Bordo vs Odontologia', '2015-10-01',default,default, 0x30);
+INSERT INTO `videos` VALUES (NULL, 0, 'ZNpMJGLHDGQ', default, 'Bordo vs Moron', 'Bordo vs Moron', 'Bordo vs Moron', '2015-10-01',default,default, 0x30);
+INSERT INTO `videos` VALUES (NULL, 0, 'Y0oX0xiwOv8', default,'Boyhood Trailer', 'Boyhood is nominated for a total of 6 Academy Awards including Best Picture, Best Actor in a Supporting Role (Ethan Hawke), Best Actress in a Supporting Role (Patricia Arquette), Best Director (Richard Linklater), Best Film Editing (Sandra Adair) and Best Original Screenplay (Richard Linklater). ', 'Boyhood | Official US Trailer | 2015 Oscar No', '2015-10-01',default,default, 0x30);
