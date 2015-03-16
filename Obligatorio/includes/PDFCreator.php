0112f2f0460fe11bdf8eafb5fc1abb2047396364
Agustin Daguerre <agustin.s.daguerre@gmail.com>
2015-03-16 19:31:35 -0300

    Pdf improved

--- Obligatorio/includes/PDFCreator.php
+++ Obligatorio/includes/PDFCreator.php
@@ -1,16 +1,19 @@
 <?php
+
 require('../includes/fpdf.php');
+
 class PDF extends FPDF {
+
 // Load data
     function LoadData($videos, $perComment, $title) {
         $this->title = $title;
         $data = array();
         if ($perComment)
             foreach ($videos as $video) {
-                $data[] = array($video['idVideo'], $video['title'], $video['commentCount'], $video['votes']);
+                $data[] = array($video['idVideo'], $video['title'], $video['views'], $video['destacado'], $video['deleted'], $video['commentCount'], $video['votes']);
             } else
             foreach ($videos as $video) {
-                $data[] = array($video['idVideo'], $video['title'], $video['rating'], $video['votes']);
+                $data[] = array($video['idVideo'], $video['title'], $video['rating'],$video['destacado'], $video['deleted'], $video['views'], $video['votes']);
             }
         return $data;
     }
@@ -22,7 +25,7 @@ class PDF extends FPDF {
         // Move to the right
         $this->Cell(80);
         // Title
-        $this->Cell(30, 10, $this->title, 1, 0, 'C');
+        $this->Cell(60, 10, $this->title, 1, 0, 'C');
         // Line break
         $this->Ln(20);
     }
@@ -36,9 +39,9 @@ class PDF extends FPDF {
         $this->SetLineWidth(.3);
         $this->SetFont('', 'B');
         // Header
-        $w = array(40, 60, 50, 30);
+        $w = array(25, 50, 20, 25, 25, 30, 20);
         for ($i = 0; $i < count($header); $i++)
-            $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', true);
+            $this->Cell($w[$i], 15, $header[$i], 1, 0, 'C', true);
         $this->Ln();
         // Color and font restoration
         $this->SetFillColor(224, 235, 255);
@@ -47,14 +50,19 @@ class PDF extends FPDF {
         // Data
         $fill = false;
         foreach ($data as $row) {
-            $this->Cell($w[0], 6, $row[0], 'LR', 0, 'C', $fill);
-            $this->Cell($w[1], 6, $row[1], 'LR', 0, 'C', $fill);
-            $this->Cell($w[2], 6, number_format($row[2]), 'LR', 0, 'C', $fill);
-            $this->Cell($w[3], 6, number_format($row[3]), 'LR', 0, 'C', $fill);
+            $this->Cell($w[0], 10, number_format($row[0]), 'LR', 0, 'C', $fill);
+            $this->Cell($w[1], 10, $row[1], 'LR', 0, 'C', $fill);
+            $this->Cell($w[2], 10, number_format($row[2]), 'LR', 0, 'C', $fill);
+            $this->Cell($w[3], 10, intval($row[3]) == 1 ? 'Yes' : 'No', 'LR', 0, 'C', $fill);
+            $this->Cell($w[4], 10, intval($row[4]) == 1 ? 'Yes' : 'No', 'LR', 0, 'C', $fill);
+            $this->Cell($w[5], 10, number_format($row[5]), 'LR', 0, 'C', $fill);
+            $this->Cell($w[6], 10, number_format($row[6]), 'LR', 0, 'C', $fill);
+
             $this->Ln();
             $fill = !$fill;
         }
         // Closing line
         $this->Cell(array_sum($w), 0, '', 'T');
     }
+
 }
--- Obligatorio/privateFunctions/generatePDF.php
+++ Obligatorio/privateFunctions/generatePDF.php
@@ -12,21 +12,21 @@ if ($_SESSION['ingreso']) {
         $sqlVideosPerRating = "SELECT * FROM videos ORDER BY rating DESC";
         if ($conn->consulta($sqlVideosPerRating)) {
             $videosPerRating = $conn->restantesRegistros();
-            $sqlPerComments = "select v.*, c3.commentCount from videos as v join (select distinct c.idVideo, commentCount from comments as c join 
+            $sqlPerComments = "select v.*, c3.commentCount from videos as v left join (select distinct c.idVideo, commentCount from comments as c join 
                                 (select idVideo, count(*) as commentCount from comments group by idVideo)
-                                as c2 on (c.idVideo = c2.idVideo) order by c2.commentCount desc) as c3 on v.idVideo = c3.idVideo;";
+                                as c2 on (c.idVideo = c2.idVideo) order by c2.commentCount desc) as c3 on v.idVideo = c3.idVideo order by c3.commentCount desc;";
             if ($conn->consulta($sqlPerComments)) {
                 $videosPerComment = $conn->restantesRegistros();
                 $pdf = new PDF();
                 // Column headings
-                $header = array('Video Id', 'Title', 'Comments Count', 'Votes');
+                $header = array('Video Id', 'Title', 'Views', 'Featured?', 'Deleted?', 'Comments', 'Votes');
                 // Data loading
                 $data = $pdf->LoadData($videosPerComment, true, "Videos per Comments");
                 $pdf->SetFont('Arial', '', 14);
                 $pdf->AddPage();
                 $pdf->FancyTable($header, $data);
 
-                $header2 = array('Video Id', 'Title', 'Rating', 'Votes');
+                $header2 = array('Video Id', 'Title', 'Rating', 'Featured?', 'Deleted?','Views', 'Votes');
                 // Data loading
                 $data = $pdf->LoadData($videosPerRating, false, "Videos per Rating");
                 $pdf->SetFont('Arial', '', 14);
@@ -41,8 +41,7 @@ if ($_SESSION['ingreso']) {
     }
 }
 
-if($response == null)
-{
+if ($response == null) {
     header('HTTP/1.1 400 Bad Request');
     echo "System Error";
 }
