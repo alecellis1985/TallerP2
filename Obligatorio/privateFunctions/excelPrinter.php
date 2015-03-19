3c2ee03f16c85218de3f5ff3494c503bced4f743
Alexander Ellis <alecellis1985@gmail.com>
2015-03-19 19:58:39 -0300

    Update

--- Obligatorio/privateFunctions/excelPrinter.php
+++ Obligatorio/privateFunctions/excelPrinter.php
@@ -10,17 +10,41 @@ if ($_SESSION['ingreso']) {
     if ($conn->conectar()) {
         $sqlVideosPerRating = "SELECT * FROM videos ORDER BY rating DESC";
         if ($conn->consulta($sqlVideosPerRating)) {
-            $videosPerRating = $conn->restantesRegistros();
+            $videosPerRatingRsult = $conn->restantesRegistros();
+            $videosPerRating = array();
+
+            foreach ($videosPerRatingRsult as $row) {
+                $elemn = array();
+                foreach ($row as $keyRow => $value) {
+                    if (!(gettype($keyRow) == 'integer')) {
+                        $elemn[$keyRow] = $value;
+                    }
+                }
+                array_push($videosPerRating, $elemn);
+            }
+            //var_dump($videosPerRating);
             $sqlPerComments = "select v.*, c3.commentCount from videos as v join (select distinct c.idVideo, commentCount from comments as c join 
                                 (select idVideo, count(*) as commentCount from comments group by idVideo)
                                 as c2 on (c.idVideo = c2.idVideo) order by c2.commentCount desc) as c3 on v.idVideo = c3.idVideo;";
             if ($conn->consulta($sqlPerComments)) {
-                $videosPerComment = $conn->restantesRegistros();
+                $videosPerCommentArr = $conn->restantesRegistros();
+                
+                $videosPerComment = array();
+                
+                foreach ($videosPerCommentArr as $row) {
+                    $elemn = array();
+                    foreach ($row as $keyRow => $value) {
+                        if (!(gettype($keyRow) == 'integer')) {
+                            $elemn[$keyRow] = $value;
+                        }
+                    }
+                    array_push($videosPerComment, $elemn);
+                }
             }
         }
         $conn->desconectar();
     }
-    // filename for download
+
     $filename = "MyCompay_Videos" . date('Ymd') . ".xls";
 
     header("Content-Disposition: attachment; filename=\"$filename\"");
