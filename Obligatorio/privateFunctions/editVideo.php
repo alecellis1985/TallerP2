a2657db14079022a77ab3ce79029a160647de48d
Agustin Daguerre <agustin.s.daguerre@gmail.com>
2015-03-19 12:52:57 -0300

    Fix check video url exist for Edit

--- Obligatorio/privateFunctions/editVideo.php
+++ Obligatorio/privateFunctions/editVideo.php
@@ -4,8 +4,7 @@ require_once("../config/parametros.php");
 require_once("../includes/class.Conexion.BD.php");
 require_once("../includes/MessageHandler.php");
 session_start();
-if($_SESSION['ingreso'])
-{
+if ($_SESSION['ingreso']) {
     $idVideo = $_POST['idVideo'];
     $title = $_POST['title'];
     $client = $_POST['client'];
@@ -16,26 +15,34 @@ if($_SESSION['ingreso'])
     $conn = new ConexionBD(DRIVER, SERVIDOR, BASE, USUARIO, CLAVE);
     $response = null;
     if ($conn->conectar()) {
-        $featuredInt = $featured ? 1 : 0;
+        //Check for video Url is not duplicated 
+        $sqlCheck = "SELECT * FROM videos WHERE url = :url";
+        $paramsCheck = array();
+        $paramsCheck[0] = array("url", $url, "STRING", 150);
+        if ($conn->consulta($sqlCheck, $paramsCheck) && $conn->cantidadRegistros() > 0) {
+            $response = MessageHandler::getErrorResponse("Video already exists");
+        } else {
+            //Check if must update featured video
+            $featuredInt = $featured ? 1 : 0;
+            $sql = "UPDATE videos SET client = :client , url = :url , releaseDate = :releaseDate , description = :description, title =:title, destacado = :featured WHERE idVideo = :idVideo ";
+            $parametros = array();
+            $parametros[0] = array("client", $client, "STRING", 200);
+            $parametros[1] = array("url", $url, "STRING", 150);
+            $parametros[2] = array("releaseDate", $releaseDate, "STRING", 250);
+            $parametros[3] = array("description", $description, "STRING", 250);
+            $parametros[4] = array("idVideo", $idVideo, "INT");
+            $parametros[5] = array("title", $title, "STRING", 45);
+            $parametros[6] = array("featured", $featuredInt, "INT");
 
-        $sql = "UPDATE videos SET client = :client , url = :url , releaseDate = :releaseDate , description = :description, title =:title, destacado = :featured WHERE idVideo = :idVideo ";
-        $parametros = array();
-        $parametros[0] = array("client", $client, "STRING", 200);
-        $parametros[1] = array("url", $url, "STRING", 150);
-        $parametros[2] = array("releaseDate", $releaseDate, "STRING", 250);
-        $parametros[3] = array("description", $description, "STRING", 250);
-        $parametros[4] = array("idVideo", $idVideo, "INT");
-        $parametros[5] = array("title", $title, "STRING", 45);
-        $parametros[6] = array("featured", $featuredInt, "INT");
+            if ($featured) {
+                $sqlUpdateAll = "UPDATE videos SET destacado = 0 WHERE idVideo <> :idVideo";
+                $parametros2 = array();
+                $parametros2[0] = array("idVideo", $idVideo, "INT");
+            }
 
-        if ($featured) {
-            $sqlUpdateAll = "UPDATE videos SET destacado = 0 WHERE idVideo <> :idVideo";
-            $parametros2 = array();
-            $parametros2[0] = array("idVideo", $idVideo, "INT");
-        }
-
-        if ($conn->consulta($sql, $parametros) && $conn->consulta($sqlUpdateAll, $parametros2)) {
-            $response = MessageHandler::getSuccessResponse('Video successfully updated', null);
+            if ($conn->consulta($sql, $parametros) && $conn->consulta($sqlUpdateAll, $parametros2)) {
+                $response = MessageHandler::getSuccessResponse('Video successfully updated', null);
+            }
         }
     }
 
@@ -46,9 +53,7 @@ if($_SESSION['ingreso'])
         $conn->desconectar();
         echo $response;
     }
-}
-else
-{
+} else {
     header('HTTP/1.1 401 Unauthorized Request');
     echo MessageHandler::getDBUnauthorizedResponse();
 }    
