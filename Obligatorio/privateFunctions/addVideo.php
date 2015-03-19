7d76b78065b4ab65140455cce25fac4d0a7b8633
Agustin Daguerre <agustin.s.daguerre@gmail.com>
2015-03-18 22:14:27 -0300

    Validation duplicate video url

--- Obligatorio/privateFunctions/addVideo.php
+++ Obligatorio/privateFunctions/addVideo.php
@@ -14,23 +14,30 @@ if ($sessionExists && $_SESSION['ingreso']) {
     $conn = new ConexionBD(DRIVER, SERVIDOR, BASE, USUARIO, CLAVE);
     $response = null;
     if ($conn->conectar()) {
-        $sql = "INSERT INTO videos (client, url, title, releaseDate, description, destacado, views, deleted)";
-        $sql .= " VALUES (:client, :url, :title, :releaseDate, :description, 0, 0, 0)";
-        $parametros = array();
-        $parametros[0] = array("client", $client, "STRING", 45);
-        $parametros[1] = array("url", $url, "STRING", 150);
-        $parametros[2] = array("title", $title, "STRING", 45);
-        $parametros[3] = array("releaseDate", $releaseDate, "STRING", 250);
-        $parametros[4] = array("description", $description, "STRING", 250);
-        /*
-         * TODO: check if video already exists
-         *          */
-        if ($conn->consulta($sql, $parametros)) {
-            $id = $conn->ultimoIdInsert();
-            $sql2 = "SELECT * FROM videos where idVideo = " . $id;
-            if ($conn->consulta($sql2)) {
-                $elemInserted = $conn->restantesRegistros();
-                $response = MessageHandler::getSuccessResponse('Video successfully added', $elemInserted);
+        $sqlCheck = "SELECT * FROM videos WHERE url = '" . $url . "'";
+        if ($conn->consulta($sqlCheck) && $conn->cantidadRegistros() > 0) {
+            $response = MessageHandler::getErrorResponse("Este video ya existe");
+        } else {
+
+            $sql = "INSERT INTO videos (client, url, title, releaseDate, description, destacado, views, deleted)";
+            $sql .= " VALUES (:client, :url, :title, :releaseDate, :description, 0, 0, 0)";
+            $parametros = array();
+            $parametros[0] = array("client", $client, "STRING", 45);
+            $parametros[1] = array("url", $url, "STRING", 150);
+            $parametros[2] = array("title", $title, "STRING", 45);
+            $parametros[3] = array("releaseDate", $releaseDate, "STRING", 250);
+            $parametros[4] = array("description", $description, "STRING", 250);
+            /*
+             * TODO: check if video already exists
+             *          */
+
+            if ($conn->consulta($sql, $parametros)) {
+                $id = $conn->ultimoIdInsert();
+                $sql2 = "SELECT * FROM videos where idVideo = " . $id;
+                if ($conn->consulta($sql2)) {
+                    $elemInserted = $conn->restantesRegistros();
+                    $response = MessageHandler::getSuccessResponse('Video successfully added', $elemInserted);
+                }
             }
         }
     }
@@ -38,8 +45,8 @@ if ($sessionExists && $_SESSION['ingreso']) {
         header('HTTP/1.1 400 Bad Request');
         echo MessageHandler::getDBErrorResponse();
     } else {
-        $conn->desconectar();
         echo $response;
+        $conn->desconectar();
     }
 } else {
     echo "Unauthorized";
